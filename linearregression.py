64ccd9bf106c1bbf0a1eeded2ec4693b58970ef4
hiennguyen26 <77690044+hiennguyen26@users.noreply.github.com>
2021-08-26 23:40:51 -0400

    linear regression test to see what affects rent

---
+++ linearregression.py
@@ -0,0 +1,26 @@
+import statsmodels.api as sm
+from statsmodels.discrete.discrete_model import Probit
+import pandas as pd
+import numpy as np
+from statsmodels.regression.linear_model import OLS
+
+#Needs a separate numerical file to analyse
+
+def run_model(df):
+    df.columns = list(map(lambda name: name.lower(), df.columns))
+    variables = ["address","price","bathrooms","bedrooms","area", "zestimate",  "days_on_zillow",	"$/sqft",	"household_median_income_on_threshold",	"most_populated","second_most_populated","third_most_populated",
+                 "median_age", "median_house_value"	,"poverty_rate"	, "unemployment_rate",	"rent_zestimate_rentals","rent_zestimate_sold"]
+    y = df["rent_zestimate"]
+    X = sm.tools.add_constant(df[variables])
+    model = OLS(y, X)
+    print(model.fit(cov_type='HC0').summary())
+
+df = pd.read_excel(
+    'zillowflipping/results/ZillowAnalysis_Threshold95K_Aug18.xlsx')   
+
+run_model(df)
+
+
+
+
+
